295e9d81f76267003d4dfc6199553652803075b3
alejoatria <alejoatria@gmail.com>
2022-07-04 23:13:03 -0400

    creado archivo counter.jsx esqueleto inicial de componente. Pasando a gitpod por error de lectura en local

---
+++ src/js/component/counter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Counter = (props) => {
+    return (
+        <div class="counter">
+            <div className="icon"><i class="fa-solid fa-clock"></i></div>
+            <div className="centenamil">0</div>
+            <div className="decenamil">0</div>
+            <div className="mil">0</div>
+            <div className="centena">0</div>
+            <div className="decena">0</div>
+            <div className="unidad">0</div>
+        </div>
+    )
+}
+
+export default Counter;
